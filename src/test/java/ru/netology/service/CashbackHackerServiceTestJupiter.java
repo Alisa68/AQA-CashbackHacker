f39e2c2d588579c6e67ca2fd3a3dfe25c55c40c1
Alisa68 <lebedeva19682@yandex.ru>
2022-02-09 17:51:08 +0300

    Added JUnit4-platform

---
+++ src/test/java/ru/netology/service/CashbackHackerServiceTestJupiter.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerServiceTestJupiter {
+    CashbackHackerService service = new CashbackHackerService();
+
+    @Test
+    public void shouldBelowLimit() {
+        int amount = 850;
+
+        int actual = service.remain(amount);
+        int expected = 150;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldLimit() {
+        int amount = 1_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAboveLimit() {
+        int amount = 1_350;
+
+        int actual = service.remain(amount);
+        int expected = 650;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
